fd83e388a537e41f648759e3e988272f31b79154
Vardan Mittal <vardanmittal000@gmail.com>
2024-08-23 14:51:41 +0530

    Tutorial1: completed done with Gauss Elimination and Back Substitution Functions

---
+++ utils/solvers.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def GaussElimination(A:np.array,b:np.array):
+    """Function that solves gauss elimination by elementry row operations
+    
+    Keyword arguments:
+    A - np.array(): matrix
+    b - np.array(): vector
+    Return: A,b
+    """
+    
+    n = len(A)
+    for i in range(n-1):
+        for j in range(i+1,n):
+            factor = float(A[j][i]/A[i][i])
+            for k in range(i,n):
+                A[j][k] -= factor * A[i][k]
+            b[j] -= factor * b[i]
+    return A,b
+
+def BackSubstitution(A:np.array,b:np.array):
+    """This function is a simple back substitution function after gauss elimination
+    
+    Keyword arguments:
+    A - np.array(): matrix
+    b - np.array(): vector
+    Return: x -> solution
+    """
+    
+    n = len(A)
+    x = np.zeros(n)
+    for i in reversed(range(n)):
+        s = 0
+        for j in range(i+1,n):
+            s += A[i][j] * x[j]
+        x[i] = (b[i] - s) / A[i][i]
+    return x
